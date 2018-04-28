3371cae4c1a8c433c9d2f0ad499a2929fd7cfece
Rodigo Rojo <rodrigo.rojo.m@me.com>
2018-04-28 12:11:35 -0500

    Creacion del Ovni

--- FlappyOvni/GameScene.swift
+++ FlappyOvni/GameScene.swift
@@ -11,6 +11,8 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var ovni: SKSpriteNode?
+    
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
@@ -19,11 +21,23 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        ovni?.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        ovni?.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 60))
         
     }
     
     func creaJuego(){
-        
+        creaOvni()
+    }
+    
+    func creaOvni(){
+        ovni = SKSpriteNode(imageNamed: "ovni")
+        ovni?.position = CGPoint(x: -150, y: 0)
+        ovni?.size = CGSize(width: 100, height: 100)
+        ovni?.physicsBody = SKPhysicsBody(texture: (ovni?.texture)!, size: (ovni?.size)!)
+        ovni?.physicsBody?.allowsRotation = false
+        ovni?.physicsBody?.isDynamic = true
+        addChild(ovni!)
     }
     
 }
