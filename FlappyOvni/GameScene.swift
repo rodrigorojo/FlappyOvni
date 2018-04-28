9e199bdf6e10c3acb896f4e72e52056ca6e697f6
Rodigo Rojo <rodrigo.rojo.m@me.com>
2018-04-28 14:20:06 -0500

    Creacion de los tubos

--- FlappyOvni/GameScene.swift
+++ FlappyOvni/GameScene.swift
@@ -13,6 +13,8 @@ class GameScene: SKScene {
     
     var ovni: SKSpriteNode?
     
+    var timerTuberia: Timer?
+    
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
@@ -28,6 +30,8 @@ class GameScene: SKScene {
     
     func creaJuego(){
         creaOvni()
+        timerTuberia = Timer.scheduledTimer(withTimeInterval: 2, repeats: true,
+                                           block: {_ in self.creaTubos()})
     }
     
     func creaOvni(){
@@ -38,6 +42,56 @@ class GameScene: SKScene {
         ovni?.physicsBody?.allowsRotation = false
         ovni?.physicsBody?.isDynamic = true
         addChild(ovni!)
+    }
+    
+    func creaTubos(){
+        let tuboSuperior = SKSpriteNode(imageNamed: "tubo1")
+        let enMedio = SKSpriteNode(color: UIColor.blue, size: CGSize(width: 15, height: 230))
+        let tuboInferior = SKSpriteNode(imageNamed: "tubo2")
+        
+        let anchoTubos = tuboSuperior.size.width
+        let altoTubos = tuboInferior.size.height*1.5
+        
+        tuboSuperior.size = CGSize(width: anchoTubos, height: altoTubos)
+        tuboSuperior.physicsBody = SKPhysicsBody(texture: tuboSuperior.texture!,
+                                                 size: tuboSuperior.size)
+        tuboSuperior.physicsBody?.affectedByGravity = false
+        tuboSuperior.physicsBody?.allowsRotation = false
+        tuboSuperior.physicsBody?.isDynamic = false
+        
+        enMedio.isHidden = true
+        enMedio.physicsBody = SKPhysicsBody(rectangleOf: enMedio.size)
+        enMedio.physicsBody?.affectedByGravity = false
+        enMedio.physicsBody?.isDynamic = true
+        
+        tuboInferior.size = CGSize(width: anchoTubos, height: altoTubos)
+        tuboInferior.physicsBody = SKPhysicsBody(texture: tuboInferior.texture!,
+                                                 size: tuboInferior.size)
+        tuboInferior.physicsBody?.affectedByGravity = false
+        tuboInferior.physicsBody?.allowsRotation = false
+        tuboInferior.physicsBody?.isDynamic = false
+        
+        let inicioX = size.width/2 + anchoTubos
+        let numeroAleatorio = CGFloat(arc4random_uniform(UInt32(3)))+2
+        tuboSuperior.position = CGPoint(x: inicioX, y: size.height/numeroAleatorio)
+        
+        enMedio.position = CGPoint(x: inicioX,
+                                   y: tuboSuperior.position.y -
+                                    tuboSuperior.size.height + 365)
+
+        let tuboInferiorY = tuboSuperior.position.y - tuboSuperior.size.height - 230
+        tuboInferior.position = CGPoint(x: inicioX, y: tuboInferiorY)
+        
+        let finX = -size.width - anchoTubos*2
+        let desplazamiento = SKAction.moveBy(x: finX, y: 0, duration: 4)
+        
+        tuboSuperior.run(SKAction.sequence([desplazamiento,SKAction.removeFromParent()]))
+        enMedio.run(SKAction.sequence([desplazamiento,SKAction.removeFromParent()]))
+        tuboInferior.run(SKAction.sequence([desplazamiento,SKAction.removeFromParent()]))
+        
+        addChild(tuboSuperior)
+        addChild(enMedio)
+        addChild(tuboInferior)
     }
     
 }
